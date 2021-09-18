d507df720e418800bf1bf9127657364637d79464
joacofum <joacofum@gmail.com>
2021-09-18 18:58:13 -0300

    borrado de cosas al pedo

--- Produccion/output/config.py
+++
@@ -1,9 +0,0 @@
-from BD.Queries import Conexion
-
-EMG = "EMG"
-DB_FILE = 'bd_humilde.db'
-ROW_COLUMNS = Conexion.get_row_columns()
-ENCODING = 'cp1252'
-FILES_CSV = 'Archivos CSV (*.csv);'
-PATH_FONTS = 'Static/fonts'
-LIMITE_GRAFICAS_POR_VISTA = Conexion.get_limite_graficas()
