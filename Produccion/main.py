d6a45db74518be861786ec85a636cea115e34e01
Leonardo <odriozola-leo@hotmail.com>
2021-10-03 12:32:07 -0300

    ajustar ventana automaticamente si hay una barra de tarea abierta xd

--- Produccion/main.py
+++ Produccion/main.py
@@ -1,7 +1,7 @@
 import numpy
 from PyQt5 import QtWidgets, QtCore, uic
-from PyQt5.QtWidgets import (QLabel, QMessageBox, QGraphicsDropShadowEffect, QMenuBar,QFileDialog,QWidget,QAction, QGraphicsScene, QGraphicsView ,QTreeWidget, QToolBar, QMenu,QComboBox, QTreeWidgetItem, QApplication, QHBoxLayout, QVBoxLayout, QPushButton, QTabWidget, QScrollArea)
-from PyQt5.QtGui import QIcon, QFont, QFontDatabase, QPixmap
+from PyQt5.QtWidgets import (QLabel, QMessageBox, QDesktopWidget,QGraphicsDropShadowEffect, QMenuBar,QFileDialog,QWidget,QAction, QGraphicsScene, QGraphicsView ,QTreeWidget, QToolBar, QMenu,QComboBox, QTreeWidgetItem, QApplication, QHBoxLayout, QVBoxLayout, QPushButton, QTabWidget, QScrollArea)
+from PyQt5.QtGui import QIcon, QFont, QFontDatabase,QGuiApplication, QPixmap, QScreen
 from PyQt5.QtCore import QSize, QEvent,QEventLoop,Qt,pyqtSignal,QPoint,QEasingCurve,QPropertyAnimation,QDir
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import *
@@ -88,7 +88,6 @@ class ventana_principal(QWidget):
         super(ventana_principal,self).__init__(parent=parent)
         self.initUI()
 
-
     def initUI(self):
         self.setWindowState(QtCore.Qt.WindowMaximized)
         self.setWindowTitle("LIBiAM")
@@ -97,6 +96,12 @@ class ventana_principal(QWidget):
         self.setLayout(QVBoxLayout())
         self.layout().setContentsMargins(0, 0, 0, 0)
         self.archivos_csv = []
+
+        # OBTENER TAMAÑO Y ANCHO DE LA PANTALLA SIN LA BARRA DE TAREAS
+        widget_xdd = QDesktopWidget()
+        rec = widget_xdd.availableGeometry(widget_xdd.primaryScreen())
+        self.setGeometry(rec.x(), rec.y(), rec.width(), rec.height())
+
         #IDENTIFICADOR ARCHIVOS
         self.id_archivo = 1
 
@@ -259,8 +264,8 @@ class ventana_principal(QWidget):
         self.widget_content.layout().setContentsMargins(0, 0, 0, 0)
         self.layout().addWidget(self.widget_content, 9)
 
-        self.width = self.widget_content.screen().geometry().width()
-        self.height = self.widget_content.screen().geometry().height()
+        self.width = rec.width()
+        self.height = rec.height()
 
         # CONTENEDOR DE TREE GRÁFICAS
         self.widget_izq = QWidget(self.widget_content)
@@ -394,12 +399,11 @@ class ventana_principal(QWidget):
     def cerrar(self):
         sys.exit()
 
+    def ventana_conf_archivos(self):
+        ventana_conf_archivos(self).exec_()
+
     def ventana_conf_vistas(self):
         ventana_conf_vistas(self).exec_()
-
-    def ventana_conf_archivos(self):
-
-        ventana_conf_archivos(self).exec_()
 
     def ventana_inicio(self):
 
@@ -587,13 +591,13 @@ class ventana_principal(QWidget):
             #aplicar_valor_picos.triggered.connect(lambda checked, item=item: self.aplicar_valores_picos())
 
             menu.addAction(remover_grafiaca)
-            menu.addAction(remover_filtro)
-            menu.addAction(aplicar_filtro)
-            menu.addAction(aplicar_valor_picos)
+            #menu.addAction(remover_filtro)
+            #menu.addAction(aplicar_filtro)
+            #menu.addAction(aplicar_valor_picos)
         elif item.parent() is None:
             print_burro = QAction("Print Burro")
             print_burro.triggered.connect(lambda checked, item=item: self.print_burro())
-            menu.addAction(print_burro)
+            #menu.addAction(print_burro)
         menu.exec_(self.treeView2.viewport().mapToGlobal(pos))
 
     def remover_grafica(self, item : tree_widget_item_vista):
@@ -1757,6 +1761,9 @@ def sacarSegundoParametroAxesSubplot(texto):
 def main():
 
     app = QApplication(sys.argv)
+    lpm = app.primaryScreen()
+    print(lpm.availableGeometry())
+
     pixmap = QPixmap(":/Static/img/splashscreenLibiam.jpg")
     splash = QSplashScreen(pixmap)
     splash.show()
